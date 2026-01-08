dab593553c0915c93676cfe4b5d2860a6dc806e9
smallketchup82 <ketchup@smallketchup.ca>
2026-01-07 21:33:34 -0500

    The header of all time

--- OpenTransitSimulator/Program.cs
+++ OpenTransitSimulator/Program.cs
@@ -1,14 +1,27 @@
-﻿using System;
+﻿// Copyright (C) 2026 smallketchup82 <ketchup@smkt.ca>.
+// This file is part of OpenTransitSimulator and licensed under the LGPL-3.0-or-later license.
+//
+// OpenTransitSimulator is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+// General Public License as published by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenTransitSimulator is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY, without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+// License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with OpenTransitSimulator.
+// If not, see <https://www.gnu.org/licenses/>.
 
-namespace OpenTransitSimulator
+using System;
+
+namespace OpenTransitSimulator;
+
+public static class Program
 {
-    public static class Program
+    [STAThread]
+    private static void Main()
     {
-        [STAThread]
-        static void Main()
-        {
-            using var game = new Game1();
-            game.Run();
-        }
+        using var game = new Game1();
+        game.Run();
     }
 }
